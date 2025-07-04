276bb7d39494028474d6fe092f1afcedcc250296
Pavan CB <pavancb.dev@gmail.com>
2025-07-04 12:10:38 +0000

    04 actions

---
+++ simple-spring-boot-app/src/test/java/com/pcb/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
